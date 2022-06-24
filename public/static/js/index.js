c934b46568df2b90fc1d9ead664e4a568b6e92c3
Amy Hwang <728amyh@gmail.com>
2022-06-24 19:35:51 -0400

    testing routes for potential match

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,1 +1,21 @@
-console.log("JS is loaded!");
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing Dashboard") },
+        { path: "/posts", view: () => console.log("Viewing Posts") },
+        { path: "/settings", view: () => console.log("Viewing Settings") },
+    ];
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path,
+
+        };
+    });
+
+    console.log(potentialMatches);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+})

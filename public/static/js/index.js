6165365b336e4fea5d49a9f416ad40224ebce44f
Amy Hwang <728amyh@gmail.com>
2022-06-28 16:51:59 -0400

    checking path

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,8 +1,15 @@
+import Dashboard from "../../views/Dashboard.js";
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
 const router = async () => {
     const routes = [
-        { path: "/", view: () => console.log("Viewing Dashboard") },
-        { path: "/posts", view: () => console.log("Viewing Posts") },
-        { path: "/settings", view: () => console.log("Viewing Settings") },
+        { path: "/", view: Dashboard },
+        // { path: "/posts", view: () => console.log("Viewing Posts") },
+        // { path: "/settings", view: () => console.log("Viewing Settings") },
     ];
 
     const potentialMatches = routes.map(route => {
@@ -13,9 +20,29 @@ const router = async () => {
         };
     });
 
-    console.log(potentialMatches);
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+
+    const view = new match.route.view();
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+
+    console.log(match.route.view());
 };
 
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
     router();
 })
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "public", "static")))
+app.use("/static", express.static(path.resolve(__dirname, "public", "static")));
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"));
